25a7397923688af45fefcc999b7dda62265914c0
makarenkoj <makarenkoj@gmail.com>
2019-07-04 23:55:25 +0300

    modified

--- test.rb
+++ test.rb
@@ -2,7 +2,6 @@
 
 class Test
   def initialize(questions_path)
-
     unless File.exist?(questions_path)
       abort "Файл не #{questions_path} найден."
     end
@@ -15,28 +14,25 @@ class Test
   end
 
   def finished?
-    return @current_question >= @question.size
+    @current_question >= @question.size
   end
 
   def next_question
     puts @question[@current_question]
-
     user_input = nil
     until user_input == 1 || user_input == 2 || user_input == 3
       puts "введите число: 1 - да, 2 - нет, 3 - иногда, и нажмите Enter"
       user_input = gets.to_i
     end
-
     if user_input == 1
       @points += 2
     elsif user_input == 3
       @points += 1
     end
-
     @current_question += 1
   end
 
   def points
-    return @points
+    @points
   end
 end
